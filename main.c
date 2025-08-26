35a75565c5bcaa1b19669b45d30080eda4365f71
AariAlma <aariyan@gmail.com>
2025-08-26 13:21:33 -0400

    Added lighting and proper scaling

---
+++ main.c
@@ -0,0 +1,143 @@
+#include "raylib.h"
+#include "raymath.h"
+#define RLIGHTS_IMPLEMENTATION
+#include "rlights.h"
+#include <math.h>
+
+#if defined(PLATFORM_WEB)
+#define GLSL_VERSION 100
+#include <emscripten/emscripten.h>
+#else
+#define GLSL_VERSION 330
+#endif
+
+void UpdateDrawFrame(void);
+
+// Initialize Window
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+static Camera3D camera = {0};
+static Model eye = {0};
+static Shader shader = {0};
+static Light lights[MAX_LIGHTS] = {0};
+static Vector3 eyePosition = {0.0f, 0.0f, 0.0f};
+static float eyeRadius = 1.0f;
+
+int main() {
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+  InitWindow(screenWidth, screenHeight, "Eye Tracker Test");
+  SetWindowMinSize(320, 240);
+
+  // Initialize Camera
+  camera.position = (Vector3){5.0f, 0.0f, 0.0f};
+  camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+  camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+  camera.fovy = 60.0f;
+  camera.projection = CAMERA_PERSPECTIVE;
+
+  // Load Model
+  eye = LoadModel("assets/20250820_website-eye_AARIALMA.glb");
+
+  // Load Shader
+  shader = LoadShader(TextFormat("assets/shaders/lighting.vert", GLSL_VERSION),
+                      TextFormat("assets/shaders/lighting.frag", GLSL_VERSION));
+  shader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(shader, "viewPos");
+  int ambientLoc = GetShaderLocation(shader, "ambient");
+  const float ambient[4] = {0.1f, 0.1f, 0.1f, 1.0f};
+  SetShaderValue(shader, ambientLoc, ambient, SHADER_UNIFORM_VEC4);
+
+  for (int i = 0; i < eye.materialCount; i++)
+    eye.materials[i].shader = shader;
+
+  // Create Light
+  lights[0] = CreateLight(LIGHT_POINT, (Vector3){0.0f, 2.0f, 0.0f},
+                          Vector3Zero(), GREEN, shader);
+  lights[1] = CreateLight(LIGHT_POINT, (Vector3){0.0f, -1.0f, -1.732f},
+                          Vector3Zero(), RED, shader);
+  lights[2] = CreateLight(LIGHT_POINT, (Vector3){0.0f, -1.0f, 1.732f},
+                          Vector3Zero(), BLUE, shader);
+
+  lights[3] = CreateLight(LIGHT_POINT, (Vector3){10.0f, 0.0f, 0.0f},
+                          Vector3Zero(), WHITE, shader);
+#if defined(PLATFORM_WEB)
+  emscripten_set_main_loop(UpdateDrawFrame, 60, true);
+#else
+  SetTargetFPS(60);
+  while (!WindowShouldClose()) {
+    UpdateDrawFrame();
+  }
+#endif
+  UnloadModel(eye);
+  UnloadShader(shader);
+  CloseWindow();
+  return 0;
+}
+
+void UpdateDrawFrame(void) {
+  // Update camera view vector for shader
+  float cameraPos[3] = {camera.position.x, camera.position.y,
+                        camera.position.z};
+  SetShaderValue(shader, shader.locs[SHADER_LOC_VECTOR_VIEW], cameraPos,
+                 SHADER_UNIFORM_VEC3);
+
+  // Toggle light
+  if (IsKeyPressed(KEY_A))
+    lights[0].enabled = !lights[0].enabled;
+  for (int i = 0; i < MAX_LIGHTS; i++)
+    UpdateLightValues(shader, lights[i]);
+
+  // Rotation Math
+  Ray mouseRay = GetScreenToWorldRay(GetMousePosition(), camera);
+  float eyeYaw = atan2f(mouseRay.direction.z, -mouseRay.direction.x);
+  float eyePitch = atan2f(mouseRay.direction.y, -mouseRay.direction.x);
+  eye.transform = MatrixRotateXYZ((Vector3){0.0f, -eyeYaw, eyePitch});
+
+  // Initialize Drawing
+  BeginDrawing();
+  ClearBackground(BLACK);
+  // 3D
+  BeginMode3D(camera);
+  BeginShaderMode(shader);
+
+  // Render Eye
+  DrawModel(eye, eyePosition, eyeRadius, GRAY);
+
+  EndShaderMode();
+  for (int i = 0; i < MAX_LIGHTS; i++) {
+    if (lights[i].enabled)
+      DrawSphereEx(lights[i].position, 0.2f, 8, 8, lights[i].color);
+    else
+      DrawSphereWires(lights[i].position, 0.2f, 8, 8,
+                      ColorAlpha(lights[i].color, 0.3f));
+  }
+  EndMode3D();
+
+  // 2D
+  DrawRectangle(10, 10, 250, 250, Fade(SKYBLUE, 0.5f));
+  DrawRectangleLines(10, 10, 250, 250, BLUE);
+
+  DrawText("Diagnostics", 20, 20, 10, BLACK);
+  DrawText(TextFormat("Mouse X Position: %.2f", GetMousePosition().x), 40, 40,
+           10, DARKGRAY);
+  DrawText(TextFormat("Mouse Y Position: %.2f", GetMousePosition().y), 40, 60,
+           10, DARKGRAY);
+  DrawText(TextFormat("Mouse Ray X Position: %.2f", mouseRay.position.x), 40,
+           80, 10, DARKGRAY);
+  DrawText(TextFormat("Mouse Ray Y Position: %.2f", mouseRay.position.y), 40,
+           100, 10, DARKGRAY);
+  DrawText(TextFormat("Mouse Ray Z Position: %.2f", mouseRay.position.z), 40,
+           120, 10, DARKGRAY);
+  DrawText(TextFormat("Mouse Ray X Direction: %.2f", mouseRay.direction.x), 40,
+           140, 10, DARKGRAY);
+  DrawText(TextFormat("Mouse Ray Y Direction: %.2f", mouseRay.direction.y), 40,
+           160, 10, DARKGRAY);
+  DrawText(TextFormat("Mouse Ray Z Direction: %.2f", mouseRay.direction.z), 40,
+           180, 10, DARKGRAY);
+  DrawText(TextFormat("Mouse Ray Pitch: %.2f", eyePitch), 40, 200, 10,
+           DARKGRAY);
+  DrawText(TextFormat("Mouse Ray Yaw: %.2f", eyeYaw), 40, 220, 10, DARKGRAY);
+
+  // Termination
+  EndDrawing();
+}
--- makefile
+++ makefile
@@ -1,3 +1,6 @@
 default: 
-	gcc -o raylibhelloworld.exe raylibhelloworld.c -I include -L lib -lraylib -lgdi32 -lwinmm
+	gcc -o main.exe main.c -I include -L lib -lraylib -lgdi32 -lwinmm
+
+web:
+	emcc -o index.html main.c -Os -Wall ./lib/libraylib.web.a  -I./include/ -L./lib/ -s USE_GLFW=3 --preload-file assets@/assets --shell-file E:/_dev/raylib/src/minshell.html -DPLATFORM_WEB
 
